87af1932ac0d923158156416ddc10a9821806472
yeomeme <yeomeme@gmail.com>
2023-01-02 16:58:55 +0900

    [add] HeaderModel 로 데이터 변경

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -8,19 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    var headers : [Header] = headerData
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            VStack(alignment: .center, spacing: 20) {
+                // MARK: - HEADER
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0){
+//                        HeaderView()
+                        ForEach(headers) { item in
+                            HeaderView(header : item)
+                        }
+                    }
+                }
+                
+                
+                // MARK: - FOOTER
+                
+                VStack(alignment: .center,spacing: 20){
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(headers : headerData)
     }
 }
--- View/HeaderView.swift
+++ View/HeaderView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct HeaderView: View {
     
     @State private var showHeadline : Bool = false
-    
+   
+    var header : Header
     var slideInAnimation : Animation {
         Animation.spring(response: 1.5, dampingFraction: 0.5, blendDuration: 0.5)
             .speed(1)
@@ -19,7 +20,7 @@ struct HeaderView: View {
     
     var body: some View {
         ZStack {
-            Image("avocado-slice-1")
+            Image(header.image)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
             
@@ -29,13 +30,13 @@ struct HeaderView: View {
                     .frame(width: 4)
                 
                 VStack{
-                    Text("Avocado")
+                    Text(header.headline)
                         .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
                         .foregroundColor(Color.white)
                         .shadow(radius: 3)
                     
-                    Text("Avocados are a powerhouse ingredient at any meal for anyone.")
+                    Text(header.subheadline)
                         .font(.footnote)
                         .lineLimit(2)
                         .multilineTextAlignment(.leading)
@@ -60,7 +61,7 @@ struct HeaderView: View {
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView(header: headerData[1])
             .environment(\.colorScheme, .dark)
     }
 }
