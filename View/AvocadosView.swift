76fcad5a0391c2b88e8f59a63413923a5540bfb3
yeomeme <yeomeme@gmail.com>
2023-01-02 16:13:48 +0900

    [add] avocadoView dark/ light mode

--- View/AvocadosView.swift
+++ View/AvocadosView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    
+//    @State private var pulsatenAnimation: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Image("avocado")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 240, height: 240 , alignment: .center)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y:8)
+            
+            VStack{
+            Text("Avocados")
+                .font(.system(size: 42, weight: .bold, design: .serif))
+                .foregroundColor(Color.white)
+                .padding()
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+            Text("""
+Creamy, green, and full of nutrients! Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+""")
+            .lineLimit(nil)
+            .font(.system(.headline, design: .serif))
+            .foregroundColor(Color("ColorGreenLight"))
+            .multilineTextAlignment(.center)
+            .lineSpacing(8)
+            .frame(maxWidth: 640, minHeight: 120)
+        }
+        .padding()
+            Spacer()
+        }
+        .background(
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            )
+        .edgesIgnoringSafeArea(.all)
+//        .onAppear()
     }
+        
 }
 
 struct AvocadosView_Previews: PreviewProvider {
     static var previews: some View {
         AvocadosView()
+            .environment( \.colorScheme, .dark)
     }
 }
