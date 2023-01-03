24a5b4912ed7369cc262b6f91787d296da215fdf
yeomeme <yeomeme@gmail.com>
2023-01-03 12:09:31 +0900

    [modi,add] contentView DataModel 로 변경 , FactView 삽입, FactView 레이아웃 위치 수정

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     // MARK: - PROPERTIES
     
     var headers : [Header] = headerData
+    var facts: [Fact] = factData
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             
@@ -33,6 +35,24 @@ struct ContentView: View {
                  
                 DishesView()
                     .frame(maxWidth: 640)
+                
+                
+                // MARK: - AVOCADO FACTS
+                Text("Avocado Facts")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                ScrollView(.horizontal,  showsIndicators: false) {
+                    HStack(alignment: .top,spacing: 60) {
+                        ForEach(facts) { item in
+                          FactsView(fact: item)
+                        }
+                    }
+                    .padding(.vertical)
+                    .padding(.leading, 60)
+                    .padding(.trailing, 20)
+                }
+                
                 // MARK: - FOOTER
                 
                 VStack(alignment: .center,spacing: 20){
@@ -66,6 +86,6 @@ struct TitleModifier: ViewModifier {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(headers : headerData)
+        ContentView(headers : headerData, facts: factData)
     }
 }
