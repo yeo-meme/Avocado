e90f5b0e0f3ebd7e4f133a38cd40c590cde211e3
yeomeme <yeomeme@gmail.com>
2023-01-03 11:59:24 +0900

    [add] fact Data / Model

---
+++ Model/Factmodel.swift
@@ -0,0 +1,14 @@
+//
+//  Factmodel.swift
+//  Abocado
+//
+//  Created by yeomim Kim on 2023/01/03.
+//
+
+import SwiftUI
+
+struct Fact: Identifiable {
+    var id = UUID()
+    var image: String
+    var content: String
+}
