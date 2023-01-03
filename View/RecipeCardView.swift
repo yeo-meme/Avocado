2dfcc356ddfa32d830f2732320b40c33a1650740
yeomeme <yeomeme@gmail.com>
2023-01-03 16:37:07 +0900

    [add] RecipeCarView -> ContentView add

--- View/RecipeCardView.swift
+++ View/RecipeCardView.swift
@@ -12,7 +12,7 @@ struct RecipeCardView: View {
     var recipe : Recipe
     
     var body: some View {
-        VStack(alignment: .leading,spacing: 0) {
+        VStack(alignment: .leading, spacing: 0) {
             Image(recipe.image)
                 .resizable()
                 .scaledToFit()
@@ -48,33 +48,11 @@ struct RecipeCardView: View {
                 
                 
                 //rates
-                RecipeCardView(recipe: recipe)
-//                HStack(alignment: .center, spacing: 5) {
-//                    ForEach(1...(recipe.rating), id: \.self) { _ in
-//                        Image(systemName: "star.fill")
-//                            .font(.body)
-//                            .foregroundColor(Color.yellow)
-//
-//                    }
-//                }
+                RecipeRatingView(recipe: recipe)
                 
                 //cooking
-                RecipeCardView(recipe: recipe)
-//                HStack(alignment: .center,spacing: 12) {
-//                    HStack(alignment: .center,spacing: 2) {
-//                        Image(systemName: "person.2")
-//                        Text("Sever: \(recipe.serves)")
-//                    }
-//                    HStack(alignment: .center,spacing: 2) {
-//                        Image(systemName: "clock")
-//                        Text("Prep: \(recipe.preparation)")
-//                    }
-//                    HStack(alignment: .center,spacing: 2) {
-//                        Image(systemName: "flame")
-//                        Text("Cooking: \(recipe.cooking)")
-//                    }
-//
-//                }
+                RecipeCookingView(recipe: recipe)
+  
             }
             .padding()
             .padding(.bottom, 12)
