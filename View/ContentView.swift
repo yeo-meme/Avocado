7d500c635fb34d026def822749691546d667cd80
yeomeme <yeomeme@gmail.com>
2023-01-03 10:46:50 +0900

    [add] avocado Dishes View 커스텀 Ui View

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -26,15 +26,19 @@ struct ContentView: View {
                     }
                 }
                 
-                
+                // MARK: - DISHES
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                 
+                DishesView()
+                    .frame(maxWidth: 640)
                 // MARK: - FOOTER
                 
                 VStack(alignment: .center,spacing: 20){
                     Text("All About Avocados")
-                        .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
-                        .foregroundColor(Color("ColorGreenAdaptive"))
-                        .padding(8)
+                        .modifier(TitleModifier())
                     
                     Text("Everything you wanted to know about avocados but were too afraid to ask.")
                         .font(.system(.body, design: .serif))
@@ -48,6 +52,15 @@ struct ContentView: View {
         }
         .edgesIgnoringSafeArea(.all)
         .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+            .foregroundColor(Color("ColorGreenAdaptive"))
+            .padding(8)
     }
 }
 
--- View/DishesView.swift
+++ View/DishesView.swift
@@ -9,22 +9,89 @@ import SwiftUI
 
 struct DishesView: View {
     var body: some View {
-        HStack() {
-            HStack() {
-                Image("icon-toasts")
-                    .resizable()
-                    .frame(width: 42, height: 42, alignment: .center)
-                Spacer()
-                Text("Toasts")
+        HStack(alignment: .center,spacing: 4)
+        {
+            //1st Colum
+            VStack(alignment: .leading, spacing: 4) {
+                HStack() {
+                    Image("icon-toasts")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Toasts")
+                }
+                Divider()
+                HStack() {
+                    Image("icon-tacos")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Toasts")
+                }
+                Divider()
+                HStack() {
+                    Image("icon-salads")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Salads")
+                }
+                Divider()
+                HStack() {
+                    Image("icon-halfavo")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Spreads")
+                }
             }
             
-            Image(systemName: "heart.circle")
-            HStack() {
-                Text("Guacamole")
-                Spacer()
-                Image("icon-guacamole")
-                    .resizable()
-                    .frame(width: 42, height: 42, alignment: .center)
+            //2nd Column
+            VStack {
+                HStack {
+                    Divider()
+                }
+                Image(systemName: "heart.circle")
+                    .imageScale(.large)
+                    .font(Font.title.weight(.ultraLight))
+                HStack {
+                    Divider()
+                }
+            }
+            
+            //3rd Column
+            VStack (alignment: .trailing, spacing: 4) {
+                HStack() {
+                    Text("Guacamole")
+                    Spacer()
+                    Image("icon-guacamole")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                Divider()
+                HStack() {
+                    Text("Sandwich")
+                    Spacer()
+                    Image("icon-sandwiches")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                Divider()
+                HStack() {
+                    Text("Soup")
+                    Spacer()
+                    Image("icon-soup")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                Divider()
+                HStack() {
+                    Text("Smoothie")
+                    Spacer()
+                    Image("icon-smoothies")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
             }
         }
         .font(.system(.callout, design: .serif))
